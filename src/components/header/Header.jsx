6b700b33bdbc8e8f1933600c0dce1e956b7d33ed
Faisal Odunuga <faisalodunuga@gmail.com>
2025-06-29 23:28:35 +0100

    fixed nav issue

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -9,7 +9,7 @@ const Header = () => {
   const { savedDishes } = useContext(AllContext);
 
   return (
-    <nav className="sticky top-0 bg-yellow-300 flex flex-col lg:flex-row lg:items-center gap-2 justify-between px-10 py-4 font-bold text-xl z-30">
+    <nav className="sticky top-0 bg-yellow-300 flex flex-col lg:flex-row lg:items-center gap-2 justify-between p-2 lg:px-10 lg:py-4 font-bold text-xl z-30">
       <h1 className="">
         <Link to="/">CookIt</Link>
       </h1>
